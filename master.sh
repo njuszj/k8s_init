456b24e7164957c04a42b02e584039121eb5254e
github_s <39424690+njuszj@users.noreply.github.com>
2023-03-17 23:41:52 +0800

    Create master.sh

---
+++ master.sh
@@ -0,0 +1,41 @@
+# ubuntu 20.04
+
+sudo passwd
+su
+
+
+# install kubeadm
+mkdir /etc/apt/keyrings
+curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg 
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get updateapt-get update
+apt-get install -y kubelet kubeadm kubectl
+
+
+# create master node
+swapoff -a
+
+cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+
+hostnamectl set-hostname master
+
+echo "Input host, use EOF to end"
+while read line; do
+  if [[ $line == "EOF" ]]; then
+    break
+  fi
+  echo "$line" >> /etc/host
+done
+
+apt install docker.io
+kubeadm reset -f
+
+echo "Input master ip: "
+read ip
+kubeadm init --apiserver-advertise-address=$ip --service-cidr=10.1.0.0/16 --pod-network-cidr=10.244.0.0/16
